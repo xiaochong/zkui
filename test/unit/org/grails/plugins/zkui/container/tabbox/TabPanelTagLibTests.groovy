33bf434ab6ee6ca347f0511b4175dfda362c183e
groovyquan <groovyquan@gmail.com>
2011-03-09 22:25:56 +0800

    unit test

---
+++ test/unit/org/grails/plugins/zkui/container/tabbox/TabPanelTagLibTests.groovy
@@ -0,0 +1,17 @@
+package org.grails.plugins.zkui.container.tabbox
+
+import grails.test.*
+
+class TabPanelTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ test/unit/org/grails/plugins/zkui/container/tabbox/TabTagLibTests.groovy
@@ -0,0 +1,17 @@
+package org.grails.plugins.zkui.container.tabbox
+
+import grails.test.*
+
+class TabTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
