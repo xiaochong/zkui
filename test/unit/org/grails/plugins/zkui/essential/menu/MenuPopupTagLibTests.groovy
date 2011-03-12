2a33bc420f120baf9dd3a59ae997cbbb755f9683
groovyquan <groovyquan@gmail.com>
2011-03-12 17:50:49 +0800

    unit test

---
+++ test/unit/org/grails/plugins/zkui/essential/menu/MenuPopupTagLibTests.groovy
@@ -0,0 +1,17 @@
+package org.grails.plugins.zkui.essential.menu
+
+import grails.test.*
+
+class MenuPopupTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
