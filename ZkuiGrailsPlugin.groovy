493fb5e6168054f75d2c83f1545755d32b367bcd
groovyquan <groovyquan@gmail.com>
2011-04-06 11:46:44 +0800

    change version to 0.2

--- ZkuiGrailsPlugin.groovy
+++ ZkuiGrailsPlugin.groovy
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional
 
 class ZkuiGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
