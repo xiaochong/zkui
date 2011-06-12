b7ca00b730dbee97a419e5179143805431c6fc46
groovyquan <groovyquan@gmail.com>
2011-06-12 22:28:32 +0800

    PlotinfoTagLib Test

---
+++ test/unit/org/grails/plugins/zkui/diagrams/timeplot/PlotinfoTagLibTests.groovy
@@ -0,0 +1,17 @@
+package org.grails.plugins.zkui.diagrams.timeplot
+
+import grails.test.*
+
+class PlotinfoTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
