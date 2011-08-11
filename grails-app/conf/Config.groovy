0c9375cf9238209117bca850f2a22bae6105946d
groovyquan <groovyquan@gmail.com>
2011-08-11 17:09:16 +0800

    change title to Grails ZK UI Plugin

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -23,10 +23,10 @@ log4j = {
     warn 'org.mortbay.log'
 }
 
-grails.doc.title = "ZK UI Plugin for Grails"
+grails.doc.title = "Grails ZK UI Plugin"
 grails.doc.authors = "groovyquan"
 grails.doc.license = "GNU Free Documentation License"
-grails.doc.copyright = "This document is licensed under GNU Free Documentation License,ZK UI Plugin for Grails is released under LGPL"
-grails.doc.footer = "ZK UI Plugin Project"
+grails.doc.copyright = "This document is licensed under GNU Free Documentation License,ZK UI Plugin is released under LGPL"
+grails.doc.footer = "Grails ZK UI Plugin Project"
 grails.doc.logo = '<a href="http://grails.org" target="_blank"><img alt="ZK UI Logo" title="ZK UI Plugin for Grails Framework" src="../img/zkui_logo.png" border="0"/></a>'
 grails.doc.sponsorLogo = '<a href="http://springsource.com" target="_blank"></a>'
