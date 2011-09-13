8fd5f8dba969d7e3298a7d513c419cfd929228de
groovyquan <groovyquan@gmail.com>
2011-09-13 22:49:47 +0800

    change version to 0.3.3

--- ZkuiGrailsPlugin.groovy
+++ ZkuiGrailsPlugin.groovy
@@ -16,7 +16,7 @@ import org.zkoss.zul.Messagebox
 
 class ZkuiGrailsPlugin {
     // the plugin version
-    def version = "0.3.2"
+    def version = "0.3.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
