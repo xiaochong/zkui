bf418dd36ca314e3150215421a75d2db6a27bcff
groovyquan <groovyquan@gmail.com>
2011-11-09 22:01:22 +0800

    add zbind dependencies, refs #35

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -28,10 +28,11 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.13'
-        def zkVersion="6.0.0.FL.20111019"
+        def zkVersion="6.0.0.FL.20111107"
         runtime "org.zkoss.zk:zk:${zkVersion}"
         runtime "org.zkoss.zk:zul:${zkVersion}"
         runtime "org.zkoss.zk:zkplus:${zkVersion}"
         runtime "org.zkoss.zk:zhtml:${zkVersion}"
+        runtime "org.zkoss.zk:zbind:${zkVersion}"
     }
 }
