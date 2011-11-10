bbf1364fde863eaaa6e6fc64594534b586cba499
groovyquan <groovyquan@gmail.com>
2011-11-11 00:00:43 +0800

    add zk 6.0 template tag

--- grails-app/taglib/org/grails/plugins/zkui/BaseTagLib.groovy
+++ grails-app/taglib/org/grails/plugins/zkui/BaseTagLib.groovy
@@ -1,8 +1,13 @@
 package org.grails.plugins.zkui
 
 import org.grails.plugins.zkui.util.Renders
+import org.zkoss.util.ArraysX
+import org.zkoss.xel.VariableResolver
 import org.zkoss.zk.fn.JspFns
 import org.zkoss.zk.ui.Component
+import org.zkoss.zk.ui.Execution
+import org.zkoss.zk.ui.Executions
+import org.zkoss.zk.ui.util.Template
 
 class BaseTagLib {
     static namespace = "z"
@@ -56,5 +61,64 @@ class BaseTagLib {
         out << b.call()
         request.removeAttribute('zk_page_id')
         request.removeAttribute('zk_page_style')
+    }
+
+    def template = {attrs, b ->
+        true || attrs.name || attrs.src
+        final Map<String, String> _params = new LinkedHashMap<String, String>()
+        String name = null, src = null;
+        attrs.each {key, value ->
+            final String attnm = key
+            final String attval = value
+            if ("name".equals(attnm)) {
+                name = attval;
+            } else if ("src".equals(attnm)) {
+                src = attval;
+            } else {
+                _params.put(attnm, attval)
+            }
+        }
+        def comp = request['parents']?.last
+        def body = b()?.toString()?.replace('#{', '${')
+        comp.setTemplate(name, new TemplateImpl(_params, src, body))
+    }
+
+    private static class TemplateImpl implements Template, Serializable {
+        final Map _params
+        final String src
+        final String body
+
+        TemplateImpl(Map _params, String src, String body) {
+            this._params = _params
+            this.src = src
+            this.body = body
+        }
+
+        Component[] create(Component parent, Component insertBefore, VariableResolver resolver) {
+            final Execution exec = Executions.getCurrent()
+            if (resolver != null) exec.addVariableResolver(resolver)
+            final Component[] cs
+            try {
+                cs = [exec.createComponentsDirectly(body, null, parent, insertBefore, resolver)] as Component[]
+            } finally {
+                if (resolver != null) exec.removeVariableResolver(resolver)
+            }
+            final Component c2 = src != null ?
+                exec.createComponents(src, parent, insertBefore, resolver) : null
+            def components = merge(cs, c2)
+            return components
+        }
+
+        Map<String, Object> getParameters() {
+            return _params
+        }
+
+        private static Component[] merge(Component[] cs, Component c) {
+            if (c != null) {
+                cs = (Component[]) ArraysX.resize(cs, cs.length + 1)
+                cs[cs.length - 1] = c
+            }
+            return cs
+        }
     }
 }
