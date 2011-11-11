44869f2d1791f6f104bfd066ce66db29e01bcc48
groovyquan <groovyquan@gmail.com>
2011-11-11 21:40:23 +0800

    use zk's Selectors replace zk's Selector set version to 0.5.M1

--- ZkuiGrailsPlugin.groovy
+++ ZkuiGrailsPlugin.groovy
@@ -8,16 +8,16 @@ import org.codehaus.groovy.grails.web.pages.TagLibraryLookup
 import org.codehaus.groovy.grails.web.util.TypeConvertingMap
 import org.grails.plugins.zkui.artefacts.composer.ComposerArtefactHandler
 import org.grails.plugins.zkui.artefacts.vm.ViewModelArtefactHandler
-import org.grails.plugins.zkui.jsoup.select.Selector
 import org.grails.plugins.zkui.metaclass.RedirectDynamicMethod
 import org.grails.plugins.zkui.util.UriUtil
 import org.zkoss.zk.ui.Executions
+import org.zkoss.zk.ui.select.Selectors
 import org.zkoss.zul.Messagebox
 import org.zkoss.zul.impl.InputElement
 
 class ZkuiGrailsPlugin {
     // the plugin version
-    def version = "0.5"
+    def version = "0.5.M1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
@@ -149,13 +149,14 @@ The different is it more likely to use the Grails' infrastructures such as gsp, 
             delegate.appendChild(value)
         }
         org.zkoss.zk.ui.Component.metaClass.select = {String query ->
-            return Selector.select(query, delegate)
+            return Selectors.find(delegate, query)
         }
         org.zkoss.zk.ui.Component.metaClass.addEventListener = {String eventName, Closure listenerClosure ->
             return delegate.addEventListener(eventName, listenerClosure as org.zkoss.zk.ui.event.EventListener)
         }
         org.zkoss.zk.ui.Component.metaClass.getParams = {
-            return delegate.select("[name]").inject(new TypeConvertingMap()) {s, c ->
+            return delegate.select("*").inject([:]) {s, c ->
+                if (!c.metaClass.respondsTo(c, 'getName')) return s
                 def e = s.get(c.name)
                 def value
                 if (c instanceof org.zkoss.zul.Combobox) {
@@ -163,11 +164,13 @@ The different is it more likely to use the Grails' infrastructures such as gsp, 
                 } else if (c instanceof org.zkoss.zul.Checkbox) {
                     value = c.value ?: c.isChecked()
                 } else if (c instanceof org.zkoss.zul.Listbox) {
-                    value = c.getSelectedItems()?.value as String[]
+                    value = c.getSelectedItems()?.value
                 } else if (c instanceof org.zkoss.zul.Radiogroup) {
                     return s
-                } else {
+                } else if (c.metaClass.respondsTo(c, 'getValue')) {
                     value = c.value
+                } else {
+                    return s
                 }
                 if (value == null) {
                     value = ''
@@ -178,6 +181,13 @@ The different is it more likely to use the Grails' infrastructures such as gsp, 
                     e << value
                 } else {
                     s.put(c.name, [s.remove(c.name), value])
+                }
+                return s
+            }.inject(new TypeConvertingMap()) {s, e ->
+                if (e instanceof Collection) {
+                    s.put(e.key, e.value as String[])
+                } else {
+                    s.put(e.key, e.value)
                 }
                 return s
             }
