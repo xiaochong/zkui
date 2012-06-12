b4755baefc35eae0b40a74cfcd68cc1170596a97
groovyquan <groovyquan@gmail.com>
2012-06-12 14:55:27 +0800

    update version to 0.5.2

--- ZkuiGrailsPlugin.groovy
+++ ZkuiGrailsPlugin.groovy
@@ -20,7 +20,7 @@ import org.zkoss.zul.impl.InputElement
 
 class ZkuiGrailsPlugin {
     // the plugin version
-    def version = "0.5.1"
+    def version = "0.5.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.2 > *"
     // the other plugins this plugin depends on
