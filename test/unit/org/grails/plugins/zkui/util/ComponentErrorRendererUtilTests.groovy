172fd04b6215dd7dac8d14118faf871f19240aa9
groovyquan <groovyquan@gmail.com>
2012-06-12 14:42:09 +0800

    import test annotation,refs pull 63

--- test/unit/org/grails/plugins/zkui/util/ComponentErrorRendererUtilTests.groovy
+++ test/unit/org/grails/plugins/zkui/util/ComponentErrorRendererUtilTests.groovy
@@ -1,38 +1,37 @@
 package org.grails.plugins.zkui.util
 
-import static org.junit.Assert.*
-import org.zkoss.zul.impl.InputElement
-import org.zkoss.zk.ui.Component
+import grails.test.ComposerUnitTestCase
+import org.junit.Before
+import org.junit.Test
 import org.zkoss.zul.Textbox
 import org.zkoss.zul.Window
-import grails.test.*
 
-class ComponentErrorRendererUtilTests extends ComposerUnitTestCase{
+class ComponentErrorRendererUtilTests extends ComposerUnitTestCase {
 
-   Window component
-   Textbox txt1
+    Window component
+    Textbox txt1
 
-   @Before
-   void setUp(){
-      Window.metaClass.select = { [txt1] }
-      component = new Window()
-      txt1 = new Textbox()
-      txt1.setName('name')
-      component.appendChild(txt1)
-   }
+    @Before
+    void setUp() {
+        Window.metaClass.select = { [txt1] }
+        component = new Window()
+        txt1 = new Textbox()
+        txt1.setName('name')
+        component.appendChild(txt1)
+    }
 
-   @Test
-   void should_add_renderMapAsErrors_to_InputElement(){
-      ComponentErrorRendererUtil util = new ComponentErrorRendererUtil()
-      util.addRenderMapAsErrors()
+    @Test
+    void should_add_renderMapAsErrors_to_InputElement() {
+        ComponentErrorRendererUtil util = new ComponentErrorRendererUtil()
+        util.addRenderMapAsErrors()
 
-      def params = [errors:[[field: 'name', error: 'Name can not be empty']]]
+        def params = [errors: [[field: 'name', error: 'Name can not be empty']]]
 
-      component.renderMapAsErrors(bean: params)
+        component.renderMapAsErrors(bean: params)
 
-      assertEquals 'Name can not be empty', txt1.getErrorMessage()
+        assertEquals 'Name can not be empty', txt1.getErrorMessage()
 
 
-   }
+    }
 
 }
